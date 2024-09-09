2aad0949612dea147cd5669b21ad3747f4f49684
ramizshamis <160166765+ramizshamis@users.noreply.github.com>
2024-09-09 20:55:48 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var personalData = {
+    name: "Ramiz",
+    age: 26,
+    cityName: "Karachi"
+};
+console.log(personalData);
+////;;;;;;;;;  Q 22  ;;;;;;;;////
+var cityName = ["Karachi", "Lahore", "Islamabad", "Peshawer"];
+console.log(cityName[5]); //intentional error
+console.log(cityName[3]);
+////;;;;;;;;;;  Q23   ;;;;;;;;////
+var car = "subaru";
+console.log("Is car == 'subaru'? I predict True."); //its just only  a message
+console.log(car == "subaru"); //compare
+console.log("Is car == 'vitz'? I predict false."); //its just only  a message
+console.log(car == "vitz");
+console.log("Is car !== 'subaru'? I predict false."); //its just only  a message
+console.log(car !== "subaru");
+console.log("Is car !== 'vitz'? I predict true."); //its just only  a message
+console.log(car !== "vitz");
+console.log("Is car == 'corola'? I predict false."); //its just only  a message
+console.log(car == "corola");
+console.log("Is car !== 'corola'? I predict true."); //its just only  a message
+console.log(car !== "corola");
+console.log("Is car !== 'civic'? I predict true."); //its just only  a message
+console.log(car !== "civic");
+console.log("Is car == 'civic'? I predict false."); //its just only  a message
+console.log(car == "civic");
+console.log("Is car !== 'Audi'? I predict true."); //its just only  a message
+console.log(car !== "Audi");
+console.log("Is car == 'Audi'? I predict false."); //its just only  a message
+console.log(car == "Audi");
