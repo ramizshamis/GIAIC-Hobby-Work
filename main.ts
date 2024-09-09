bc52df7fe07b5b659e71ad138478dfe639d332ca
ramizshamis <160166765+ramizshamis@users.noreply.github.com>
2024-09-09 20:55:12 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,57 @@
+interface personalData{
+    name : string;
+    age : number;
+    cityName : string;
+}
+let personalData ={
+    name : "Ramiz",
+    age : 26,
+    cityName :"Karachi"
+}
+console.log(personalData);
+
+
+
+
+
+////;;;;;;;;;  Q 22  ;;;;;;;;////
+
+let cityName:string[] =["Karachi","Lahore","Islamabad","Peshawer"];
+console.log(cityName[5]); //intentional error
+
+console.log(cityName[3]);
+
+
+
+
+
+
+
+
+////;;;;;;;;;;  Q23   ;;;;;;;;////
+
+
+let car ="subaru";
+console.log("Is car == 'subaru'? I predict True.");//its just only  a message
+console.log(car =="subaru");   //compare
+console.log("Is car == 'vitz'? I predict false.");//its just only  a message
+console.log(car =="vitz"); 
+console.log("Is car !== 'subaru'? I predict false.");//its just only  a message
+console.log(car !=="subaru"); 
+console.log("Is car !== 'vitz'? I predict true.");//its just only  a message
+console.log(car !=="vitz"); 
+console.log("Is car == 'corola'? I predict false.");//its just only  a message
+console.log(car =="corola"); 
+console.log("Is car !== 'corola'? I predict true.");//its just only  a message
+console.log(car !=="corola"); 
+console.log("Is car !== 'civic'? I predict true.");//its just only  a message
+console.log(car !=="civic"); 
+console.log("Is car == 'civic'? I predict false.");//its just only  a message
+console.log(car =="civic"); 
+console.log("Is car !== 'Audi'? I predict true.");//its just only  a message
+console.log(car !=="Audi"); 
+console.log("Is car == 'Audi'? I predict false.");//its just only  a message
+console.log(car =="Audi"); 
+
+
+
